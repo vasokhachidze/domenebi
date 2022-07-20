b887c7a44bb1199bbb0710c67ffd2496db5956b6
vasokhachidze <khachidzev239@gmail.com>
2022-07-20 17:05:37 +0400

    add

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const domens = document.querySelectorAll('.domens');
 const nameSearch = document.getElementById('name-seacrh');
 const sBtn = document.getElementById('btn-div')
 
+// checkboxes
+const checkBoxes = document.querySelectorAll('.filter ul li input')
 
 // price inputs
 const startPrice = document.querySelector('#price-start input')
@@ -15,6 +17,29 @@ const endPrice = document.querySelector('#price-end input')
 // simbol inputs
 const simbolStart = document.querySelector('.start-simbol input');
 const simbolEnd = document.querySelector('.end-simbol input');
+
+
+// checkbox search
+for(let checkbox of checkBoxes){
+    checkbox.addEventListener('click',()=>{
+        domens.forEach(domen =>{
+            if(checkbox.checked == true){
+                if(domen.classList.contains(checkbox.id)){
+                    domen.style.display = 'block'
+                }else{
+                    domen.style.display = 'none'
+                }
+                
+            }else{
+                domen.style.display = 'block'
+            }
+        })
+        
+    })
+}
+for(chec in checkBoxes){
+    console.log(chec);
+}
 
 
 // listener 
@@ -27,12 +52,12 @@ sBtn.addEventListener('click',(e)=>{
     const searchValue = nameSearch.value.toLowerCase();
     
     // price search input new values
-    const priceSearchStart = Number(startPrice.value);
-    const priceSearchEnd = Number(endPrice.value);
+    let priceSearchStart = Number(startPrice.value);
+    let priceSearchEnd = Number(endPrice.value);
 
     // simbol search input new values
-    const simbolSearchStart = Number(simbolStart.value)
-    const simbolSearchEnd = Number(simbolEnd.value)
+    let simbolSearchStart = simbolStart.value
+    let simbolSearchEnd = simbolEnd.value
 
     // search by name loop
     domens.forEach(domen =>{
@@ -48,11 +73,12 @@ sBtn.addEventListener('click',(e)=>{
     })
     // search by price loop
     empty.forEach(empty =>{
-        if(priceSearchStart == ''){
-            empty.style.display = 'block'
-        }else if(priceSearchStart <= Number(empty.ariaValueNow) & priceSearchEnd >= Number(empty.ariaValueNow)){
+         if(priceSearchStart <= Number(empty.ariaValueNow) & priceSearchEnd >= Number(empty.ariaValueNow)){
             empty.style.display = 'block'
             arr.push(empty)
+            
+        }else if(priceSearchEnd == ''){
+           priceSearchEnd = 100000;
         }else{
             empty.style.display = 'none'
         }
@@ -60,10 +86,10 @@ sBtn.addEventListener('click',(e)=>{
        })
     // search by simbol loop
     arr.forEach(ar =>{
-        if(simbolSearchStart == ''){
+         if (ar.dataset.filter.length  >= simbolSearchStart & ar.dataset.filter.length  <=simbolSearchEnd){
             ar.style.display = 'block'
-        }else if (ar.dataset.filter.length >= simbolSearchStart & ar.dataset.filter.length <=simbolSearchEnd){
-            ar.style.display = 'block'
+        }else if(simbolSearchEnd == ''){
+           simbolSearchEnd = 100;
         }else{
             ar.style.display = 'none'
         }
@@ -74,4 +100,46 @@ sBtn.addEventListener('click',(e)=>{
 
 
 
+showMeDomenInfo();
 
+function showMeDomenInfo(){
+    domens.forEach(domen =>{
+        const trigger = domen.querySelector('.domens-box');
+        const icon = domen.querySelector('.fa-chevron-down');
+        const addCart = domen.querySelector('.showme-price');
+        
+    
+        trigger.addEventListener('click',()=>{
+            trigger.nextElementSibling.classList.toggle('active')
+            icon.classList.toggle('active-icon');
+            
+            
+            
+        })
+    
+        const iconCheck = document.createElement('i');
+        iconCheck.className = 'fa fa-check';
+    
+        const h2 = document.createElement('h2');
+        h2.textContent = 'კალათშია';
+    
+        const returnIcon = document.createElement('i');
+        returnIcon.className = 'fa-solid fa-cart-shopping';
+        addCart.addEventListener('click',()=>{
+            addCart.classList.toggle('showme')
+            
+            if(addCart.classList.contains('showme')){
+               addCart.querySelector('.fa-cart-shopping').remove()
+               addCart.append(iconCheck)
+               addCart.append(h2)
+            }else{
+                iconCheck.remove()
+                h2.remove()
+                addCart.append(returnIcon)
+            } 
+        })
+    })
+    
+}
+
+// firstP.setAttribute("id","firstPracPara");
